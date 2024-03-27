57783be07b6b7f7d14e056a704419a838c9f0ace
vascsx <vasczn@gmail.com>
2024-03-27 00:39:22 -0300

    feat: quarta questão do desafio

---
+++ Questao3/TerceiraQuestao.cs
@@ -0,0 +1,8 @@
+/*Com base na lógica fornecida na questão 
+
+a o próximo numero é 9 , este esta somando o numero com 2
+b o próximo é 128 = aqui é o dobro 32 o dobro 64, o próximo é 128
+c o próximo é 49, é o numero ao quadrado 5x5 = 25, 6X6 = 36, logo o próximo seria 7X7 = 49
+d o próximo é 128, lógica seria 2 elevado a quantidade numeros como o próximo numero é 2 elevado a 7, logo é 128
+e o próximo é 13, porque é o anterior mais o próximo, mas essa começou depois que tinham os dois que ai repetiu e veio 1+1= 2, 2+1 =3 , 3+2 = 5, 5+3= 8, 8+5 = 13
+/* f o próximo é 27, estamos adicionando 8 ao primeiro número, 2, e 2 ao segundo, 10, e assim por diante. Então, o próximo número seria 19 + 8 = 27.
